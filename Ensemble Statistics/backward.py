a8c181f15b10386afa1803fbac3a8107629e45bd
Z.D.Yu <yuzd610@163.com>
2024-09-04 05:06:54 +0800

    Add files via upload

---
+++ Ensemble Statistics/backward.py
@@ -0,0 +1,139 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from scipy.misc import derivative
+import numpy as np
+from scipy.stats import norm
+from scipy.integrate import quad
+import matplotlib.pyplot as plt
+from scipy.misc import derivative
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+import numdifftools as nd
+n = 100000
+
+pi1 = 0.5
+pi2 = 0.5
+mu1 = 1
+mu2 = -1
+sigma_2 = 0.01
+num = 200
+ds = 0.01
+Iterate = 120
+T = ds*Iterate
+
+
+def Stot(t):
+    def sample_mixture_gaussian(t):
+
+        assert np.isclose(pi1 + pi2, 1), "The sum of pi1 and pi2 must be 1."
+
+        dist_selection = np.random.choice(['dist1', 'dist2'], size=n, p=[pi1, pi2])
+
+        samples = np.zeros(n)
+
+        for i, dist in enumerate(dist_selection):
+            if dist == 'dist1':
+                samples[i] = np.random.normal(mu1 * np.exp(-t),
+                                              np.sqrt((1 - np.exp(-2 * t) + sigma_2 * np.exp(-2 * t))))
+            else:
+                samples[i] = np.random.normal(mu2 * np.exp(-t),
+                                              np.sqrt((1 - np.exp(-2 * t) + sigma_2 * np.exp(-2 * t))))
+
+        return samples
+
+    def P(x, t):
+
+        return 0.5 * norm.pdf(x, loc=mu1 * np.exp(-t), scale=np.sqrt(1 - np.exp(-2 * t) + sigma_2 * np.exp(-2 * t))) + \
+            0.5 * norm.pdf(x, loc=mu2 * np.exp(-t), scale=np.sqrt(1 - np.exp(-2 * t) + sigma_2 * np.exp(-2 * t)))
+
+
+    sample = sample_mixture_gaussian(t)
+    var = 1 -np.exp(-2 * t)+sigma_2*np.exp(-2 * t)
+    scale = mu1 * mu1 * np.exp(-2 * t)
+    mean = np.tanh(mu1*np.exp(-t)/var*sample)**2
+    mean1 = np.mean(mean)
+
+    pi = scale + var -2 - scale/(var**2) + scale/(var**2)*mean1 + 1/var
+    phi = scale + var -3-2*scale/(var**2) + 2*scale/(var**2)*mean1 + 2/var
+
+
+
+
+
+
+
+
+
+
+
+
+    return pi, phi
+
+
+t_valuesS = np.linspace(0, T, num)
+
+
+S_values_Sol = [Stot(t)[0] for t in tqdm(t_valuesS)]
+S_values_phi_b = [Stot(t)[1]for t in tqdm(t_valuesS)]
+
+
+
+
+
+
+
+
+
+
+S_values_Rate = [a - b for a, b in zip(S_values_Sol,S_values_phi_b)]
+
+
+ts = -np.log(1/(sigma_2+1))/2
+ts =round(ts,8)
+min_value = min(S_values_phi_b)
+min_index = S_values_phi_b.index(min_value)
+t_min = min_index *(T/(num-1))
+
+fig, ax1 = plt.subplots(figsize=(10, 6))
+color = 'tab:red'
+ax1.set_xlabel('t', color='black', fontsize=24)  # 增大xlabel字体大小
+ax1.set_ylabel('$value$', color='black', fontsize=24)  # 增大ylabel字体大小
+
+
+ax1.plot(t_valuesS, S_values_phi_b, label=r'${\phi^*}(t)$', color=color, linewidth=2)
+ax1.plot(t_valuesS, S_values_Sol, label=r'${\pi^*}(t)$', color='tab:orange', linewidth=2)
+ax1.plot(t_valuesS, S_values_Rate, label=r'${{\dot S}^*}(t)$', color='tab:olive', linewidth=2)
+
+ax1.tick_params(axis='y', labelcolor='black', labelsize=20)  # 增大y轴刻度字体大小
+ax1.tick_params(axis='x', labelcolor='black', labelsize=20)  # 增大x轴刻度字体大小
+
+
+
+plt.axvline(x=ts, color='tab:green', linestyle='--', label=f'${{t_s}}$ = {ts}', linewidth=2)
+plt.axhline(y=0, color='tab:brown', linestyle='--', linewidth=2)
+
+
+plt.axvline(x= t_min, color='purple', linestyle='--', label=f'$t^*=\\arg\\min _t  \, \phi^*(t)$ ', linewidth=2)
+
+
+
+fig.tight_layout()  # 调整布局
+plt.subplots_adjust(top=0.9)  # 调整顶部空间以避免标题被挡住
+# 增加图例字体大小
+
+
+fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes, fontsize='x-large')  # 增大图例字体大小
+
+plt.savefig('figure081.pdf')
+
+plt.show()
+
+
+
+
+
+
+
+
+
---
+++ Numerical verification of the integral fluctuation theorem/forward_n.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+pi1 = 0.5
+pi2 = 0.5
+mu1 = -1
+mu2 = 1
+sigma1 = 1
+sigma2 = 1
+dt = 0.01
+Iterate = 1000
+
+def sample_mixture_gaussian(pi1, mu1, sigma1, pi2, mu2, sigma2, n_samples):
+
+    assert np.isclose(pi1 + pi2, 1), "The sum of pi1 and pi2 must be 1."
+
+
+    dist_selection = np.random.choice(['dist1', 'dist2'], size=n_samples, p=[pi1, pi2])
+
+
+    samples = np.zeros(n_samples)
+
+
+    for i, dist in enumerate(dist_selection):
+        if dist == 'dist1':
+            samples[i] = np.random.normal(mu1, sigma1)
+        else:
+            samples[i] = np.random.normal(mu2, sigma2)
+
+    return samples
+
+
+
+N = []
+
+Mean =[]
+
+
+
+
+
+
+for n in range(0,10001,100):
+    N.append(n)
+    X = np.zeros((n, Iterate + 1))
+
+    X_0 = sample_mixture_gaussian(pi1, mu1, sigma1, pi2, mu2, sigma2, n_samples=n)
+
+    X[:, 0] = X_0
+
+    for i in range(0, Iterate):
+        X[:, i + 1] = X[:, i] - X[:, i] * dt + np.random.normal(0, 1, n) * np.sqrt(dt) * np.sqrt(2)
+
+
+    def S(X0, XT, mu_star, T):
+        return np.log(
+            (np.exp(2 * X0 * mu_star) + 1) * np.exp(
+                -0.5 * X0 ** 2 - X0 * mu_star + 0.5 * XT ** 2 + XT * mu_star * np.exp(
+                    -T) - 0.5 * mu_star ** 2 + 0.5 * mu_star ** 2 * np.exp(-2 * T)) /
+            (np.exp(2 * XT * mu_star * np.exp(-T)) + 1)
+        )
+
+
+    def Sm(matrix, dt):
+
+        # 计算差分 / dt
+        diff_dt = np.diff(matrix, axis=1) / dt
+
+        # 计算 (X(n) + X(n + 1)) / 2
+        avg_x = (matrix[:, :-1] + matrix[:, 1:]) / 2
+        instant_power = -diff_dt * avg_x
+
+        # 计算总和
+        result = np.sum(instant_power, axis=1) * dt
+        return result
+
+
+    Sm = Sm(X, dt)
+
+    S = S(X[:, 0], X[:, -1], mu2, dt * Iterate)
+
+    S_tol = Sm + S
+
+    mean = np.exp(-S_tol)
+    Mean.append(np.mean(np.exp(-S_tol)))
+
+
+print(N)
+
+
+
+print(Mean)
+
+
+
+
+
+
+
+plt.plot(N, Mean, marker='o',color='salmon')
+plt.xlabel('number of trajectories',fontsize=25)
+plt.ylabel('$\left\langle {{e^{ - \Delta {S_{tot}}}}} \\right\\rangle $',fontsize=25)  # 纵轴标签
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.grid(True)  
+plt.tight_layout()
+plt.savefig('forward_number.pdf')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
